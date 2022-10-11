279493f477c8b88914ab6b08a860c42d0575ae85
MitchelUTexas <114682124+MitchelUTexas@users.noreply.github.com>
2022-10-10 23:02:48 -0500

    Update PleiadesQuery.sql

--- PleiadesQuery.sql
+++ PleiadesQuery.sql
@@ -2,7 +2,8 @@ select
     gs.source_id, gs.ra, gs.ra_error, gs.dec, gs.dec_error, gs.parallax, gs.parallax_error, gs.pmra, gs.pmra_error, gs.pmdec, gs.pmdec_error,
     gs.radial_velocity, gs.radial_velocity_error,
     gs.phot_g_mean_mag, gs.phot_bp_mean_mag, gs.phot_rp_mean_mag,
-    ap.mh_gspspec, ap.mh_gspspec_lower, ap.mh_gspspec_upper, ap.fem_gspspec, ap.fem_gspspec_lower, ap.fem_gspspec_upper
+    ap.mh_gspspec, ap.mh_gspspec_lower, ap.mh_gspspec_upper, ap.fem_gspspec, ap.fem_gspspec_lower, ap.fem_gspspec_upper,
+    ap.ag_gspphot, ap.ag_gspphot_lower, ap.ag_gspphot_upper, ap.mg_gspphot, ap.mg_gspphot_lower, ap.mg_gspphot_upper
 from
     gaiadr3.gaia_source as gs
 join gaiadr3.astrophysical_parameters as ap using (source_id)
@@ -12,4 +13,4 @@ where
     and (gs.phot_rp_mean_mag is not Null)
     and (1 = CONTAINS(
         POINT(56.85, 24.1167),
-        CIRCLE(gs.ra, gs.dec, 300./60.)))
+        CIRCLE(gs.ra, gs.dec, 120./60.)))

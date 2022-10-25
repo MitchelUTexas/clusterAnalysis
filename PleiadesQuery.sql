27f7ed12b27d2e09af8458664de2c6e309af1a3d
MitchelUTexas <114682124+MitchelUTexas@users.noreply.github.com>
2022-10-25 16:19:50 -0500

    query cepheids

--- PleiadesQuery.sql
+++ PleiadesQuery.sql
@@ -3,10 +3,12 @@ select
     gs.radial_velocity, gs.radial_velocity_error,
     gs.phot_g_mean_mag, gs.phot_bp_mean_mag, gs.phot_rp_mean_mag,
     ap.mh_gspspec, ap.mh_gspspec_lower, ap.mh_gspspec_upper, ap.fem_gspspec, ap.fem_gspspec_lower, ap.fem_gspspec_upper,
-    ap.ag_gspphot, ap.ag_gspphot_lower, ap.ag_gspphot_upper, ap.mg_gspphot, ap.mg_gspphot_lower, ap.mg_gspphot_upper
+    ap.ag_gspphot, ap.ag_gspphot_lower, ap.ag_gspphot_upper, ap.mg_gspphot, ap.mg_gspphot_lower, ap.mg_gspphot_upper,
+    cep.PF, cep.PF_ERROR, cep.INT_AVERAGE_G, cep.INT_AVERAGE_G_ERROR
 from
     gaiadr3.gaia_source as gs
 join gaiadr3.astrophysical_parameters as ap using (source_id)
+left join gaiadr3.vari_cepheid as cep using (source_id)
 where
     (gs.phot_g_mean_mag is not Null)
     and (gs.phot_bp_mean_mag is not Null)
